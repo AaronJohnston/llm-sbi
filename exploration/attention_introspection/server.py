a7e07774f2358c280a2585c3ddf9375dc508e1ee
Aaron Johnston <aaron.johnston001@gmail.com>
2024-03-17 23:46:13 -0700

    Working post and response

--- exploration/attention_introspection/server.py
+++ exploration/attention_introspection/server.py
@@ -1,5 +1,6 @@
+from typing import Annotated
 from pydantic import BaseModel
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -10,10 +11,12 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse(
-        request=request, name="index.html", context={"id": id}
+        name="index.html", context={"request": request}
     )
 
 
 @app.post("/api/attention", response_class=HTMLResponse)
-def get_attention(request: Request):
-    return {"test": True}
+def get_attention(request: Request, prompt: Annotated[str, Form()]):
+    return templates.TemplateResponse(
+        name="attention.html", context={"request": request, "prompt": prompt}
+    )
